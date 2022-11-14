9be1652336cdcc4afa5a494ae59c8d30d95e3484
eth5 <29051616+eth5@users.noreply.github.com>
2022-11-14 23:04:18 +1000

    too lazy to write

--- dvornik_scene/cleaner/broom/broom.gd
+++ dvornik_scene/cleaner/broom/broom.gd
@@ -1,19 +1,29 @@
 extends Node2D
 
-const TWEEN_DURATION: float = 0.4
+const TWEEN_DURATION: float = 0.2
+const TurnSide = preload("res://dvornik_scene/cleaner/player/player.gd").TurnSade
 
-onready var clear_area:Area2D
+var player: Node2D
+var current_tween:SceneTreeTween
 
 
-func set_clear_area(clear_area:Area2D):
-	self.clear_area = clear_area
+
+func set_player(player:Node2D) -> void:
+	self.player = player
 
 
 func clear() -> void:
-	global_position = clear_area.global_position
-	$AnimationPlayer.play("clear")
+	global_position = player.get_clear_area().global_position
 	twin_show()
 	$BroomArea/CollisionShape2D.disabled = false
+	show_animation()
+
+func show_animation():
+	match player.get_turn_side():
+		TurnSide.LEFT:
+			$AnimationPlayer.play("left_clear")
+		TurnSide.RIGHT:
+			$AnimationPlayer.play("right_clear")
 
 
 func cleared() -> void:
@@ -22,17 +32,21 @@ func cleared() -> void:
 
 
 func twin_show(duration: float = TWEEN_DURATION) -> SceneTreeTween:
-	var tween:SceneTreeTween = create_tween()
-	tween.set_trans(Tween.TRANS_CUBIC)
-	tween.tween_property(self, "modulate:a", 1.0, duration)
-	return tween
+	if current_tween != null:
+		current_tween.kill()
+	current_tween = create_tween()
+	current_tween.set_trans(Tween.TRANS_CUBIC)
+	current_tween.tween_property(self, "modulate:a", 1.0, duration)
+	return current_tween
 
 
 func twin_hide(duration: float = TWEEN_DURATION) -> SceneTreeTween:
-	var tween:SceneTreeTween = create_tween()
-	tween.set_trans(Tween.TRANS_CUBIC)
-	tween.tween_property(self, "modulate:a", 0.0, duration)
-	return tween
+	if current_tween != null:
+		current_tween.kill()
+	current_tween = create_tween()
+	current_tween.set_trans(Tween.TRANS_CUBIC)
+	current_tween.tween_property(self, "modulate:a", 0.0, duration)
+	return current_tween
 
 
 func _on_Area2D_body_entered(body: Node) -> void:
