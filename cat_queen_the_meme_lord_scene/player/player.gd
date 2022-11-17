2b9db4bc3d5e3c2fbff2f72bb099c12d2a09787c
eth5 <29051616+eth5@users.noreply.github.com>
2022-11-17 21:57:25 +1000

    flash

--- cat_queen_the_meme_lord_scene/player/player.gd
+++ cat_queen_the_meme_lord_scene/player/player.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 signal player_contacted
 
+const INVULNERABILITY_TIME: float = 1.0
 const EMIT_TIMER: float = 0.2
 const DIRECTION_LEFT:float = -1.0
 const DIRECTION_RIGHT:float = 1.0
@@ -18,6 +19,9 @@ var direction : Vector2
 var contact:bool = false
 var time_between_emit_signal:float = 0
 
+onready var flasher:Node = $Flash
+onready var invulnerability:bool = false
+onready var invulnerability_timer:float = INVULNERABILITY_TIME
 
 func _physics_process(delta: float) -> void:
 	direction.x = float(Input.is_action_pressed("ui_right")) - float(Input.is_action_pressed("ui_left"))
@@ -36,8 +40,13 @@ func _physics_process(delta: float) -> void:
 	elif direction.x < 0:
 		transform.x.x = -abs(transform.x.x)
 
+
 func _process(delta: float) -> void:
-	if contact:
+	if invulnerability:
+		invulnerability_timer -= delta
+		if invulnerability_timer <= 0:
+			invulnerability = false
+	elif contact:
 		time_between_emit_signal += delta
 		if time_between_emit_signal >= EMIT_TIMER:
 			time_between_emit_signal = 0.0
@@ -46,9 +55,15 @@ func _process(delta: float) -> void:
 
 
 func _on_Area_body_entered(body: Node) -> void:
+	if invulnerability:
+		return
 	contact = true
 	time_between_emit_signal = 0.0
 	emit_signal("player_contacted")
-
+	flasher.flash(INVULNERABILITY_TIME)
+	invulnerability = true
+	invulnerability_timer = INVULNERABILITY_TIME
+	
+	
 func _on_Area_body_exited(body: Node) -> void:
 	contact = false
