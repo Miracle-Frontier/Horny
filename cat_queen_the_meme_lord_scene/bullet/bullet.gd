29209c249eb2963e3bf204585c05b41422d28f7f
eth5 <29051616+eth5@users.noreply.github.com>
2022-11-19 16:32:42 +1000

    smoketrail

--- cat_queen_the_meme_lord_scene/bullet/bullet.gd
+++ cat_queen_the_meme_lord_scene/bullet/bullet.gd
@@ -4,19 +4,32 @@ extends Area2D
 var direction:Vector2 = Vector2.RIGHT
 var speed: float = 10
 
+onready var smoketrail:Line2D = $Smoketrail
+
+func _ready() -> void:
+	smoketrail.connect("end_smoke", self, "queue_free")
+
 
 func _physics_process(delta: float) -> void:
 	position += direction * speed
-	rotate(0.1)
+	smoketrail.gravity = direction * -1
 	
-	
+
+func _process(delta: float) -> void:
+	if speed == 0:
+		modulate.a *= 0.95
+
 func _on_Bullet_body_entered(body: Node) -> void:
 	if body.has_method("damage"):
 			body.damage(1.0)
 	else:
-		print(str(body) + " no has damage method!")	
-	queue_free()
+		print(str(body) + " no has damage method!")
+	speed = 0
+	remove_child($Sprite)
+	remove_child($CollisionShape2D)
+	#queue_free()
+
 
 
 func _on_VisibilityNotifier2D_screen_exited() -> void:
-	queue_free()
+	speed = 0
